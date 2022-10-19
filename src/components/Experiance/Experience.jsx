4e8801bf30bc9559381ca43d75bf4ecfb5706f96
unknown <pankajkumar9ptl@gmail.com>
2022-10-20 00:17:16 +0530

    Experience component completed

--- src/components/Experiance/Experience.jsx
+++ src/components/Experiance/Experience.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
-
+import React from 'react';
+import './Experience.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="experience_container">
+        <div className="experince_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>REACT</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>TAILWIND</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+
+        </div>
+        <div className="exprience_backend">
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>NODEJS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>MONGODB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>MYSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experiencez-details'>
+              <BsPatchCheckFill className = 'experiencez_details-icon' />
+              <div>
+                <h4>PYTHON</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,99 @@
-import React from 'react'
-
+import React from 'react';
+import './Services.css';
+import { BiCheck } from 'react-icons/bi';
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className='service'>
+          <div className='service_head'>
+            <h3> UI / UX Design</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <biCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of UI/UX */}
+        <article className='service'>
+          <div className='service_head'>
+            <h3> WEB DEVELOPMENT</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <biCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of WEB DEVELOPMENT */}
+        <article className='service'>
+          <div className='service_head'>
+            <h3> CONTENT CREATION</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+            <li>
+              <biCheck className='service_list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, similique.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Content Creation */}
+      </div>
+    </section>
   )
 }
 
