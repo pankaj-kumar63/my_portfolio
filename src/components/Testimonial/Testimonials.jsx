5250dc7f0db2e163426e4c2f5eeb2dc416b486b2
unknown <pankajkumar9ptl@gmail.com>
2022-10-26 00:23:47 +0530

    testimonials carousel completed

--- src/components/Testimonial/Testimonials.jsx
+++ src/components/Testimonial/Testimonials.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import './Testimonials.css';
 // import IMG from '../../assets/me.png';
 import data from '../Testimonial/Data';
+// import Swiper core and required modules
+import { Pagination} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials_container">
+      <Swiper className="container testimonials_container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
         {
           data.map(({ idx, avatar, name, review }) => {
             return (
-              <article key={idx} className='testimonial'>
+              <SwiperSlide key={idx} className='testimonial'>
                 <div className="client_avatar">
                   <img src={avatar} alt="avatar image" />
                 </div>
@@ -19,11 +31,11 @@ const Testimonials = () => {
                 <small className='client_review'>
                   {review}
                 </small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
